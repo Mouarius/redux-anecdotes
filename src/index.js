455c39f64021f0ff4d242c4093987f688a9bfb6c
mouarius <mouarius@gmail.com>
2021-01-09 19:59:23 +0100

    store.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import store from './store'
 import { Provider } from 'react-redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
-
-const store = createStore(reducer, composeWithDevTools())
 
 ReactDOM.render(
   <Provider store={store}>
